08f9c185afee22b726d3b9dc5ce1aa5640735d18
dixego <dailexgo@gmail.com>
2016-08-30 18:55:48 -0500

    Esqueleto de FileManager y FileManagerException

---
+++ FileManagerException.java
@@ -0,0 +1,8 @@
+package Taqueria;
+
+public class FileManagerException extends RuntimeException{
+
+	public FileManagerException(Sting message){
+		super(message);
+	}
+}
---
+++ src/Taqueria/FileManager.java
@@ -0,0 +1,8 @@
+package Taqueria;
+
+import java.util.*;
+import java.io.*;
+
+public class FileManager{
+			
+}
