4c936802263cf84437701ec8ba3fe7ce0b8fe8e0
Diego Carrillo <dailexgo@gmail.com>
2016-09-01 20:07:26 -0500

    Implementado demasiadas cosas para describirlas todas aquí

---
+++ ClientFileIO.java
@@ -0,0 +1,160 @@
+package Taqueria;
+
+import java.lang.reflect.Array;
+import java.util.*;
+import java.io.*;
+
+public class ClientFileIO {
+	private String filename;
+	private int nextID;
+	public ArrayList<Client> clientList;
+	
+	public ClientFileIO(String filename) {
+		this.filename = filename;
+		clientList = new ArrayList<Client>();
+		nextID = 0;
+		
+		try {
+			// Set up file reading
+			File file = new File(filename);
+						
+			FileReader in = new FileReader(file.getAbsolutePath());
+			
+			// Read the file
+			BufferedReader br = new BufferedReader(in);
+			String line;
+			
+			// Add to ArrayList
+			while ((line = br.readLine()) != null) {
+				// Tokenize string to read id and name separately
+				StringTokenizer st = new StringTokenizer(line);
+				
+				// Assume first token is id number
+				nextID = Integer.parseInt(st.nextToken());
+								
+				// Paste together name
+				String name  = "";
+			
+				while (st.hasMoreTokens())
+					name += st.nextToken() + " ";
+				
+				// Remove trailing space
+				name = name.trim();
+				
+				// Create client
+				Client curr = new Client(nextID, name);
+								
+				// Add the client to the ArrayList, note
+				// that client ID will correspond to
+				// ArrayList index.
+				clientList.add(curr);
+			}
+			
+			// Get ready for next client
+			++nextID;
+			
+			br.close();
+		} catch (IOException e) {
+			// Print error if file could not be read
+			e.printStackTrace();
+		}
+	}
+	
+	public Client getClient(int id) {
+		Iterator<Client> iter = clientList.iterator();
+		
+		while (iter.hasNext()){
+			Client curr = iter.next();
+			
+			if(curr.getID() == id)
+				return curr;
+		}
+		
+		return null;
+	}
+
+	public ArrayList<Client> getClients() {
+		return clientList;
+	}
+	
+	public void addClient(String toAdd) {
+		Iterator<Client> iter = clientList.iterator();
+		
+		while (iter.hasNext()){
+			
+			if(iter.next().getName().equals(toAdd))
+				return;
+		}
+		
+		//System.out.println(toAdd + nextID);
+		Client add = new Client(nextID, toAdd);
+		
+		clientList.add(add);
+		
+		++nextID;
+	}
+	
+	public void removeClient(Client toRemove) {
+		if(clientList.get(clientList.size() - 1).equals(toRemove))
+			++nextID;
+		
+		clientList.remove(toRemove);
+	}
+	
+	public void removeClient(int id) {
+		Iterator<Client> iter = clientList.iterator();
+		
+		while (iter.hasNext()){
+			Client curr = iter.next();
+			
+			if(curr.getID() == id) {
+				clientList.remove(curr);
+				
+				if(!iter.hasNext())
+					++nextID;
+					
+				return;
+			}
+		}
+	}
+	
+	public void removeClient(String name) {
+		Iterator<Client> iter = clientList.iterator();
+		
+		while (iter.hasNext()){
+			Client curr = iter.next(); 
+			
+			if(curr.getName().equals(name)) {
+				clientList.remove(curr);
+				
+				if(!iter.hasNext())
+					++nextID;
+				
+				return;
+			}
+		}
+	}
+	
+	// No changes will be saved unless this method is called
+	public void writeChanges() {
+		
+		try {
+			File file = new File(filename);
+
+			// Create BufferedWriter
+			FileWriter out = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(out);
+			
+			// Write a line for every element in ArrayList
+			for(int i = 0; i != clientList.size(); ++i) {
+				bw.write(clientList.get(i).getID() + " " + clientList.get(i).getName());
+				bw.newLine();
+			}
+			
+			bw.close();
+		} catch (IOException e) {
+			// Print error if file could not be written
+			e.printStackTrace();
+		}
+	}
+}
---
+++ MenuFileIO.java
@@ -0,0 +1,126 @@
+
+package Taqueria;
+
+import java.util.*;
+import java.util.regex.Pattern;
+import java.io.*;
+
+public class MenuFileIO {
+    private final String filename;
+    
+    public MenuFileIO(String filename){
+        this.filename = filename;
+    }
+    
+    public ArrayList<Menu> getOrder(String dish){
+        ArrayList<Menu> menu = new ArrayList<>();
+        try {
+            // Set up file reading
+            File file = new File(filename);
+			
+            FileReader in = new FileReader(file.getAbsolutePath());
+			
+            // Read the file
+            BufferedReader br = new BufferedReader(in);
+            String line;
+			
+            while ((line = br.readLine()) != null && !line.equals("")) {
+                // Tokenize string with split, since we have delimiters in place
+                String[] tokens = line.split(Pattern.quote(","));
+								
+                // Check if it's the desired dish
+		if  (tokens[0].equals(dish)){
+                    // Use tokens to create a Menu object
+                    Menu dish2 = new Menu(Integer.parseInt(tokens[0]),
+                    tokens[1],
+                    Double.parseDouble(tokens[2]));
+					
+                    menu.add(dish2);
+		}
+            }
+            br.close();
+	} catch (IOException e) {
+            // Print error if file could not be read
+            e.printStackTrace();
+	}
+	return menu;
+    }
+
+    //Return ArrayList of all menu orders
+    public ArrayList<Menu> getOrders() {
+        ArrayList<Menu> menu = new ArrayList<>();
+        try {
+            // Set up file reading
+            File file = new File(filename);
+
+            FileReader in = new FileReader(file.getAbsolutePath());
+
+            // Read the file
+            BufferedReader br = new BufferedReader(in);
+            String line;
+
+            while ((line = br.readLine()) != null && !line.equals("")) {
+                // Tokenize string with split, since we have delimiters in place
+                String[] tokens = line.split(Pattern.quote(","));
+
+                // Use tokens to create a Menu object
+                Menu dish2 = new Menu(Integer.parseInt(tokens[0]),
+                        tokens[1],
+                        Double.parseDouble(tokens[2]));
+                menu.add(dish2);
+            }
+            br.close();
+        } catch (IOException e) {
+            // Print error if file could not be read
+            e.printStackTrace();
+        }
+        return menu;
+    }
+
+    public void changePrice(Menu toChange){
+         ArrayList<Menu> menu = new ArrayList<>();
+        try{
+            File fileOld = new File(filename);
+            FileReader in = new FileReader(fileOld.getAbsolutePath());
+
+            // Read the file
+            BufferedReader br = new BufferedReader(in);
+            String line;
+            
+             while ((line = br.readLine()) != null && !line.equals("")) {
+                // Tokenize string with split, since we have delimiters in place
+                String[] tokens = line.split(Pattern.quote(","));
+								
+                // Check if it's the desired dish
+		if  (tokens[1].equals(toChange.getDish())){
+                    // Use tokens to find the dish to replace.
+                    menu.add(toChange);
+		}else{
+                    Menu dish2 = new Menu(Integer.parseInt(tokens[0]),
+                    tokens[1],
+                    Double.parseDouble(tokens[2]));
+					
+                    menu.add(dish2);
+                }
+            }
+            br.close();
+            fileOld.delete();
+
+            File fileNew = new File(filename);
+
+            //Create BufferedWritter
+            FileWriter out = new FileWriter(fileNew.getAbsoluteFile(), true);
+            BufferedWriter bw = new BufferedWriter(out);
+            
+            for(Menu dish : menu){
+                bw.write(dish.toString());  
+            }
+            
+            bw.close();
+            
+        }catch(IOException e){
+            // Print error if file could not be written
+            e.printStackTrace();
+        }
+    }
+}
